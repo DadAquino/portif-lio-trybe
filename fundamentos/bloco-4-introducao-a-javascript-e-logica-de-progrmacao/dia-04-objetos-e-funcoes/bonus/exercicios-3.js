d83965ec2e37b4b0314b398f50b0e3bdf2575668
David <david.aquino.exeutivo@gmail.com>
2022-08-23 23:42:03 -0300

    organizando os arquivos, e metade dos excercicios bonus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-progrmacao/dia-04-objetos-e-funcoes/bonus/exercicios-3.js
@@ -0,0 +1,59 @@
+function listaDeFrutas (fruit){
+  // declaracao de variaveis para usar no loop  
+    
+    let lista = [];
+    let frutas = [];
+
+
+// loop para atribuir lista a nossa variável
+
+    for(let key in fruit){
+        frutas[key]=fruit[key];
+    }
+
+// loop para correr o array e adicionar palavras novas ao novo array
+
+    for (let i=0;i<frutas.length;i+=1){
+
+        for(let j=0;j<=lista.length;j+=1){
+
+            if(frutas[i]!==lista[j]){
+
+                lista.push(frutas[i]);  }}}
+    
+    console.log(lista);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+
+  listaDeFrutas(basket);
