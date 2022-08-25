356f8106bbf34e56a868b082eb8b9cce7c9e64db
David <david.aquino.exeutivo@gmail.com>
2022-08-25 15:52:33 -0300

    exercicios de prática

---
+++ fundamentos/bloco-5-javaScript-DOM-eventos-webStorage/dia-01-DOM-e-seletores/pratica/script.js
@@ -0,0 +1,8 @@
+document.getElementById("header-container").style.backgroundColor = 'green';
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = 'pink';
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = 'yellow';
+document.querySelector('.emergency-tasks h3').style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple';
+document.querySelector('.no-emergency-tasks h3').style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+document.getElementById('footer-container').style.backgroundColor = "green";
