c5c4cb4d58719a83dfa5374d0432f58b4ad684f6
David <david.aquino.exeutivo@gmail.com>
2022-08-29 17:30:29 -0300

    primeiro exercicio feito

--- fundamentos/bloco-5-javaScript-DOM-eventos-webStorage/dia-03-eventos/exercicios/script.js
+++ fundamentos/bloco-5-javaScript-DOM-eventos-webStorage/dia-03-eventos/exercicios/script.js
@@ -17,6 +17,10 @@ function createDaysOfTheWeek() {
 
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+let dias = document.querySelector('#days');
 for (let i = 0; i<decemberDaysList.length; i+=1 ){
-    
+    let n = document.createElement('li');
+    n.className = 'day';
+    n.innerText = decemberDaysList[i];
+    dias.appendChild(n);
 }
