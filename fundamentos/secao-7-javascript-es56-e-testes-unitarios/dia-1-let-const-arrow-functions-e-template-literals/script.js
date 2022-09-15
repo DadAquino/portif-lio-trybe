f22f6725fd08433e386e2ba1d06882a9cc54d030
David <david.aquino.exeutivo@gmail.com>
2022-09-15 17:46:22 -0300

    parte 1 terminada

--- fundamentos/secao-7-javascript-es56-e-testes-unitarios/dia-1-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/secao-7-javascript-es56-e-testes-unitarios/dia-1-let-const-arrow-functions-e-template-literals/script.js
@@ -1,13 +1,21 @@
-  function testingScope(escopo) {
+  const testingScope = escopo => {
     if (escopo === true) {
       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
       console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+    console.log(`${ifScope} o que estou fazendo aqui ? :O'`); // Se necessário esta linha pode ser removida;
   }
 
   testingScope(true);
+
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const sortOddsAndEvens = (e) => e.sort();
+  console.log(sortOddsAndEvens(oddsAndEvens));
+  // será necessário alterar essa linha 😉
+
