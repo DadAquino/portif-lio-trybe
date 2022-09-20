ec6a644d55f9fe8c15c0e782a3f918ca159b3568
David <david.aquino.exeutivo@gmail.com>
2022-09-19 21:11:50 -0300

    exercicios de 1 a 5 feitos

---
+++ fundamentos/secao-7-javascript-es56-e-testes-unitarios/dia-3-primeiros-passos-em-jest/parte-2/nao-retorna.test.js
@@ -0,0 +1,7 @@
+const arr = require('./arr');
+
+describe('testes da função arr', () => {
+    it('teste de retorno correto', () => {
+        expect(arr([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    });
+});
